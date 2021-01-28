f2f77f2d1e95d40d01038cfdf51ea179dfc3ad7f
lukasz mazurek <lukaszmazurek.contact@gmail.com>
2021-01-28 15:24:20 +0100

    Changes to be committed: 	modified:   app/models.py

---
+++ app/models.py
@@ -0,0 +1,10 @@
+"""
+Put your models here
+To create databse follow the commands.
+*Run python shell in managment_app folder
+*from app import database.db
+*exit()
+This will create all your models and put
+your databse in app folder
+"""
+
